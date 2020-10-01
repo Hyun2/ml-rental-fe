08c1f4dd0049a3c14d0d56b4782196b0da7d7b44
Hyun2 <hks0284@gmail.com>
2020-10-01 21:37:44 +0900

    배포 준비

---
+++ src/pages/members.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MemberTable from "../components/MemberTable";
+import Wrapper from "../components/Wrapper";
+
+export default ({ current, setCurrent }) => {
+  const [members, setMembers] = useState();
+  // console.log(setCurrent);
+
+  useEffect(() => {
+    if (!members) {
+      getMembers();
+    }
+  });
+
+  const getMembers = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_URL}members/?page=1`
+      );
+      setMembers(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return members ? (
+    <Wrapper>
+      <MemberTable
+        setMembers={setMembers}
+        counts={members.count}
+        results={members.results}
+      />
+      {/* {console.log(members)} */}
+    </Wrapper>
+  ) : null;
+};
